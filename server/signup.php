52e86c587deccef062815662a280ebb4ba624b11
tanliuyuan <tanliuyuan@aliyun.com>
2016-03-20 23:26:50 -0500

    calendar

--- server/signup.php
+++ server/signup.php
@@ -3,18 +3,18 @@ require_once('database.php');
 
 // Fetch, validate, and assign input data
 if(isset($_POST)) {
-	if(!empty($_POST['username'])
+	if(!empty($_POST['username']))
 		$username = preg_match('/^[A-Za-z0-9_]{3,16}$/', $_POST['username']) ? $_POST['username'] : "";
 	if(empty($username)) {
 		header('HTTP/1.1 500 Internal Server Error');
         header('Content-Type: application/json');
         die(json_encode(array('error' => 'username not set')));
     }
-	if(!empty($_POST['first_name'])
+	if(!empty($_POST['first_name']))
 		$firstName = preg_match('/[A-Za-z ]{1,50}/', $_POST['first_name']) ? $_POST['first_name'] : "";
-	if(!empty($_POST['last_name'])
+	if(!empty($_POST['last_name']))
 		$lastName = preg_match('/[A-Za-z ]{1,50}/', $_POST['last_name']) ? $_POST['last_name'] : "";
-	if(!empty($_POST['password'])
+	if(!empty($_POST['password']))
 		$password = preg_match('/^[A-Za-z0-9_-]{6,18}$/', $_POST['password']) ? $_POST['password'] : "";
 	if(empty($password)) {
 		header('HTTP/1.1 500 Internal Server Error');
