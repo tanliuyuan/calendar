17ad23659e407c7cde4d2fee86ca61d95b26568a
tanliuyuan <tanliuyuan@aliyun.com>
2016-03-20 23:29:17 -0500

    calendar

--- server/signup.php
+++ server/signup.php
@@ -11,9 +11,9 @@ if(isset($_POST)) {
         die(json_encode(array('error' => 'username not set')));
     }
 	if(!empty($_POST['first_name']))
-		$firstName = preg_match('/[A-Za-z ]{1,50}/', $_POST['first_name']) ? $_POST['first_name'] : "";
+		$firstName = preg_match('/[A-Za-z\ ]{1,50}/', $_POST['first_name']) ? $_POST['first_name'] : "";
 	if(!empty($_POST['last_name']))
-		$lastName = preg_match('/[A-Za-z ]{1,50}/', $_POST['last_name']) ? $_POST['last_name'] : "";
+		$lastName = preg_match('/[A-Za-z\ ]{1,50}/', $_POST['last_name']) ? $_POST['last_name'] : "";
 	if(!empty($_POST['password']))
 		$password = preg_match('/^[A-Za-z0-9_-]{6,18}$/', $_POST['password']) ? $_POST['password'] : "";
 	if(empty($password)) {
@@ -26,7 +26,7 @@ if(isset($_POST)) {
 }
 
 // Add new user into database 
-$stmt = $mysqli->prepare("INSERT INTO users (username, first_name, last_name, hashed_password, is_admin) values (?, ?, ?, ?, 1)");
+$stmt = $mysqli->prepare("INSERT INTO users (username, first_name, last_name, hashed_password) values (?, ?, ?, ?)");
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
 	exit;
