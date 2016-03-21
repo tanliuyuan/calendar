6e673ae54324ff075187137a42bdcfe9fdbc0541
tanliuyuan <tanliuyuan@aliyun.com>
2016-03-21 00:17:32 -0500

    calendar

--- server/signup.php
+++ server/signup.php
@@ -8,18 +8,28 @@ if(isset($_POST)) {
 	if(empty($username)) {
 		header('HTTP/1.1 500 Internal Server Error');
         header('Content-Type: application/json');
-        die(json_encode(array('error' => 'username not set')));
+        die(json_encode(array('error' => 'username not valid')));
     }
 	if(!empty($_POST['first_name']))
 		$firstName = preg_match('/[A-Za-z \'-]{1,50}/', $_POST['first_name']) ? $_POST['first_name'] : "";
+	if(empty($firstName)) {
+		header('HTTP/1.1 500 Internal Server Error');
+        header('Content-Type: application/json');
+        die(json_encode(array('error' => 'first name not valid')));
+    }
 	if(!empty($_POST['last_name']))
 		$lastName = preg_match('/[A-Za-z \'-]{1,50}/', $_POST['last_name']) ? $_POST['last_name'] : "";
+	if(empty($lastName)) {
+		header('HTTP/1.1 500 Internal Server Error');
+        header('Content-Type: application/json');
+        die(json_encode(array('error' => 'last name not valid')));
+    }
 	if(!empty($_POST['password']))
 		$password = preg_match('/^[A-Za-z0-9_-]{6,18}$/', $_POST['password']) ? $_POST['password'] : "";
 	if(empty($password)) {
 		header('HTTP/1.1 500 Internal Server Error');
         header('Content-Type: application/json');
-        die(json_encode(array('error' => 'password not set')));
+        die(json_encode(array('error' => 'password not valid')));
 	} else
 		// Encrypt password
 		$hashed_password = crypt($password);
