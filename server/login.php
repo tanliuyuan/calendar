9815da4bbfabe503cfd29ba85c01c8fe54f5b725
tanliuyuan <tanliuyuan@aliyun.com>
2016-03-21 11:16:25 -0500

    calendar

--- server/login.php
+++ server/login.php
@@ -2,6 +2,20 @@
 require_once('database.php');
 require_once('error.php');
 
+// Fetch, validate, and assign input data
+if(isset($_POST)) {
+	if(!empty($_POST['username']))
+		$username = preg_match('/^[A-Za-z0-9_-]{3,16}$/', $_POST['username']) ? $_POST['username'] : "";
+	if(empty($username))
+		returnError('username not valid');
+	if(!empty($_POST['password']))
+		$password = preg_match('/^[A-Za-z0-9_-]{6,18}$/', $_POST['password']) ? $_POST['password'] : "";
+	if(empty($password))
+		returnError('password not valid');
+	else
+		login($username, $password);
+}
+
 function returnSuccess($JSONArray) {
 	header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
