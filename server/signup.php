f30ba0a46756ef6f5a48d14718e40d787f789ab4
tanliuyuan <tanliuyuan@aliyun.com>
2016-03-20 23:13:54 -0500

    calendar

--- server/signup.php
+++ server/signup.php
@@ -5,16 +5,22 @@ require_once('database.php');
 if(isset($_POST)) {
 	if(!empty($_POST['username'])
 		$username = preg_match('/^[A-Za-z0-9_]{3,16}$/', $_POST['username']) ? $_POST['username'] : "";
-	if(empty($username))
-		die('Fatal: username not set!');
+	if(empty($username)) {
+		header('HTTP/1.1 500 Internal Server Error');
+        header('Content-Type: application/json');
+        die(json_encode(array('error' => 'username not set');
+    }
 	if(!empty($_POST['first_name'])
 		$firstName = preg_match('/[A-Za-z ]{1,50}/', $_POST['first_name']) ? $_POST['first_name'] : "";
 	if(!empty($_POST['last_name'])
 		$lastName = preg_match('/[A-Za-z ]{1,50}/', $_POST['last_name']) ? $_POST['last_name'] : "";
 	if(!empty($_POST['password'])
 		$password = preg_match('/^[A-Za-z0-9_-]{6,18}$/', $_POST['password']) ? $_POST['password'] : "";
-	if(empty($password))
-		die('Fatal: password not set!');
+	if(empty($password)) {
+		header('HTTP/1.1 500 Internal Server Error');
+        header('Content-Type: application/json');
+        die(json_encode(array('error' => 'password not set');
+	}
 	else
 		// Encrypt password
 		$hashed_password = crypt($password);
