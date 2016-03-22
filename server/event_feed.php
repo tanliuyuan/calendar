62b870a0f8f9d70408e9f18d066d83ca014ebd6c
tanliuyuan <tanliuyuan@aliyun.com>
2016-03-22 09:02:45 -0500

    calendar events

--- server/event_feed.php
+++ server/event_feed.php
@@ -1,7 +1,10 @@
 <?php
 $array = array(
-	'title' => 'Event1',
-	'start' => '2016-03-11T03:45:40+00:00'
+	'1' => array(
+		'title' => 'Event1',
+		'start' => '2016-03-11T03:45:40+00:00',
+		'end' => '2016-03-11T04:45:40+00:00'
+	)
 );
 echo(json_encode($array));
 ?>
