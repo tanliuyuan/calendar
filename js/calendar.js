1a2da7029be37f32b119c3480cfc0d101388cd46
tanliuyuan <tanliuyuan@aliyun.com>
2016-03-23 17:24:42 -0500

    calendar events

--- js/calendar.js
+++ js/calendar.js
@@ -196,8 +196,8 @@ $(document).ready(function () {
             view = view;
             var event_id = calEvent.id;
             var event_title = calEvent.title;
-            var event_start_time = calEvent.start.format('YYYY-MM-DD hh:mm');
-            var event_end_time = calEvent.end.format('YYYY-MM-DD hh:mm');
+            var event_start_time = calEvent.start.format('YYYY-MM-DD HH:mm');
+            var event_end_time = calEvent.end.format('YYYY-MM-DD HH:mm');
             $('#edit_delete_event_modal').modal('show');
             // Edit event
             $('#edit_event').click(function (event) {
