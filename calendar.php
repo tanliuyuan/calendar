f535b4da6ec898a10c7ca5e01ec4f2eec51c1037
tanliuyuan <tanliuyuan@aliyun.com>
2016-03-22 23:08:25 -0500

    calendar events

--- calendar.php
+++ calendar.php
@@ -298,15 +298,15 @@ $(document).ready(function () {
 
 <!-- Edit/delete event options -->
 <div class="container modal fade" id="edit_delete_event_modal">
-	<div class="modal-content col-md-4 col-md-offset-4">
+	<div class="modal-content col-md-2 col-md-offset-5">
 		<div class="modal-header">
 			<button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
 		</div>
 		<div class="modal-body">
 			<div class="row">
-				<div class="col-md-4 col-md-offset-4">
-					<button id="edit_event" class="btn btn-primary center-block" type="button">Edit Event</button>
-					<button id="delete_event" class="btn btn-primary center-block" type="button">Delete Event</button>
+				<div class="col-md-2 col-md-offset-5">
+					<button id="edit_event" class="btn btn-primary" type="button">Edit Event</button>
+					<button id="delete_event" class="btn btn-primary" type="button">Delete Event</button>
 				</div>
 			</div>
 			<div class="modal-footer">
